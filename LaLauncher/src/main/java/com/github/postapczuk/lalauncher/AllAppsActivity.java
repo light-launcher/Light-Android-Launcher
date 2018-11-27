4eeffe6022ab13b7ae00d933190fe9e93cd7f6c8
Jan-Christoph Borchardt <hey@jancborchardt.net>
2018-11-27 01:50:21 +0100

    Dim system bars (API level 14), fix #13
    Signed-off-by: Jan-Christoph Borchardt <hey@jancborchardt.net>

--- LaLauncher/src/main/java/com/github/postapczuk/lalauncher/AllAppsActivity.java
+++ LaLauncher/src/main/java/com/github/postapczuk/lalauncher/AllAppsActivity.java
@@ -1,6 +1,8 @@
 package com.github.postapczuk.lalauncher;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -18,6 +20,12 @@ public class AllAppsActivity extends AppsActivity {
         setContentView(listView);
         ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) listView.getLayoutParams();
         layoutParams.leftMargin = 100;
+
+        // Dim the system bars (API level 14)
+        // https://developer.android.com/training/system-ui/dim#java
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+        }
     }
 
     @Override
