3d534ce507f8322c397c831963eaccf6e3fff3fa
Piotr Ostapczuk <postapczuk@gmx.com>
2018-12-08 10:27:10 +0100

    task-11 - swipe animation

--- LaLauncher/src/main/java/com/github/postapczuk/lalauncher/AllAppsActivity.java
+++ LaLauncher/src/main/java/com/github/postapczuk/lalauncher/AllAppsActivity.java
@@ -1,15 +1,26 @@
 package com.github.postapczuk.lalauncher;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
+import android.transition.Slide;
+import android.view.Gravity;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
 public class AllAppsActivity extends AppsActivity {
 
+    @SuppressLint("RtlHardcoded")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            this.getWindow().requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS);
+            this.getWindow().setEnterTransition(new Slide(Gravity.RIGHT));
+            this.getWindow().setExitTransition(new Slide(Gravity.LEFT));
+        }
         packageManager = getPackageManager();
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
         createNewListView();
@@ -18,7 +29,10 @@ public class AllAppsActivity extends AppsActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        this.finish();
+        finish();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+        }
     }
 
     @Override
--- LaLauncher/src/main/java/com/github/postapczuk/lalauncher/AppsActivity.java
+++ LaLauncher/src/main/java/com/github/postapczuk/lalauncher/AppsActivity.java
@@ -108,9 +108,10 @@ abstract class AppsActivity extends Activity implements Activities {
         WindowManager windowManager = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
         if (windowManager != null) {
             Display display = windowManager.getDefaultDisplay();
-            int heightViewBasedTopPadding = 10;
-            if (getTotalHeightOfListView() < display.getHeight()) {
-                heightViewBasedTopPadding = (display.getHeight() / 2) - (getTotalHeightOfListView() / 2);
+            final int displayHeight = display.getHeight();
+            int heightViewBasedTopPadding = displayHeight / 6;
+            if (getTotalHeightOfListView() < displayHeight - displayHeight / 6) {
+                heightViewBasedTopPadding = (displayHeight / 2) - (getTotalHeightOfListView() / 2);
             }
             int widthViewBasedLeftPadding = (display.getWidth() / 6);
             listView.setPadding(widthViewBasedLeftPadding, heightViewBasedTopPadding, 0, 0);
