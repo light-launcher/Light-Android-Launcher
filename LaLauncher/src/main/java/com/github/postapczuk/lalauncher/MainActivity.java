79118aaed460ea504b250a6ebebe3b5137627372
Piotr <postapczuk@gmx.com>
2019-03-13 22:52:23 +0000

    revert the key name
    Revert because if it is changed, users that had favs would loose them.

--- LaLauncher/src/main/java/com/github/postapczuk/lalauncher/MainActivity.java
+++ LaLauncher/src/main/java/com/github/postapczuk/lalauncher/MainActivity.java
@@ -30,7 +30,7 @@ import static android.R.layout.simple_list_item_1;
 
 public class MainActivity extends AppsActivity {
 
-    private static final String FAVS = "favorites";
+    private static final String FAVS = "favourites";
     private static final String UPDATE_ALERT_SHOWN = "updateAlertShownVersion";
     private static final String SEPARATOR = ",,,";
     private static final String ADD_APPLICATION = "+ Add favorite app";
